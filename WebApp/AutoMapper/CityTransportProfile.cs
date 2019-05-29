f12ec55c8cf55c933aaa11531da54a18c31aae77
DaliborOletic <doletic1@gmail.com>
2019-05-29 23:45:17 +0200

    Added automapper for easier mapping.

---
+++ WebApp/AutoMapper/CityTransportProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using WebApp.Models;
+using WebApp.Persistence.Models;
+using static WebApp.AutoMapper.AutoMapperResolver;
+
+namespace WebApp.AutoMapper
+{
+    public class CityTransportProfile : Profile
+    {
+        public CityTransportProfile()
+        {
+            CreateMap<StationDbModel, Station>()
+                .ForMember(dest => dest.Lines, opts => opts.MapFrom(s => ResloveStationLinesToLines(s.StationLines)));
+
+            CreateMap<LineDbModel, Line>()
+                .ForMember(dest => dest.Stations, opts => opts.MapFrom(s => ResloveStationLinesToStations(s.StationLines)));
+        }
+    }
+}
