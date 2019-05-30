2d13a50597b92250db3933be3fb6dbdc42953d91
serbulas1 <serbulas1@gmail.com>
2019-05-30 18:42:57 +0200

    google map working

--- Angular-app/src/app/pages/line-map/line-map.component.ts
+++ Angular-app/src/app/pages/line-map/line-map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-line-map',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./line-map.component.css']
 })
 export class LineMapComponent implements OnInit {
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
 
   constructor() { }
 
   ngOnInit() {
+    this.gmapInit();
+  }
+
+  gmapInit() {
+    const mapProp = {
+      center: new google.maps.LatLng(45.2700573, 19.837304),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
   }
 
 }
