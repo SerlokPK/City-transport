e9873fffecf9b908dc0022e21888eed6c7769f21
serbulas1 <serbulas1@gmail.com>
2019-10-12 16:16:23 +0200

    crated admin controller and moved admin methods to it

---
+++ WebApp/Helpers/ErrorHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Web.Http.ModelBinding;
+using WebApp.Models;
+
+namespace WebApp.Helpers
+{
+    public static class ErrorHelper
+    {
+        public static Error GetErrorMessage(ModelStateDictionary modelState)
+        {
+            var errorMessage = modelState.Values.ToList()[0].Errors.FirstOrDefault(e => e.ErrorMessage != "")?.ErrorMessage;
+            return new Error()
+            {
+                ErrorMessage = errorMessage ?? "Bad request."
+            };
+        }
+    }
+}
